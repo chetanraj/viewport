aa2551119f38f2fcd9fc294a56a43085a2839e3d
chetan <chetan.narian@gmail.com>
2016-03-06 20:07:25 +0530

    Added Android & Tablet Version & some more changes
    1. Added Android & Tablet Version
    2. Added Global Events

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-
+	
 	$('.d-w').text( screen.width + 'px' );
 	$('.d-h').text( screen.height + 'px' );
 	
 	/*
 	 * For Mac
-	 To-Do - Find the iPhone Version
+	 * To-Do - Find the Mac Version
 	 */
 	Modernizr.on('mac', function(result) {
 		$('.card-row.hidden').removeClass('hidden');
@@ -16,28 +16,42 @@ $(document).ready(function() {
 	
 	/*
 	 * For iPhone
-	 To-Do - Find the iPhone Version
+	 * To-Do - Find the iPhone Version
 	 */
 	Modernizr.on('iphone', function(result) {
 		$('.card-row.hidden').removeClass('hidden');
 		
 		$('.d').text('iPhone');
-		$('.d').before('<i class="icono-iphoneBold"></i>');
-		
-		// Listen for orientation changes
-		window.addEventListener("orientationchange", function() {
+		$('.d').before('<i class="icono-iphoneBold icon-device"></i>');
+	});
+	
+	/*
+	 * For Android
+	 * To-Do - Find the Device Type
+	 */
+	Modernizr.on('android', function(result) {
+		if( Modernizr.tablet ) {
+			$('.card-row.hidden').removeClass('hidden');
 			
-			if($('.portrait').length) {
-				$('.icono-iphoneBold').addClass('rotate');
-			}	else {
-				$('.icono-iphoneBold').removeClass('rotate');
-			}
-			
-		}, false);
+			$('.d').text('Android Tablet');
+			$('.d').before('<i class="icono-nexus icon-device"></i>');
+		}
 	});
 
+	/*
+	 * Global Events
+	 */
 	$(window).resize(function() {
 		$('.d-w').text( $(window).width() + 'px' );
 		$('.d-h').text( $(window).height() + 'px' );
 	});
+	
+	window.addEventListener("orientationchange", function() {
+		if($('.portrait').length) {
+			$('.icon-device').addClass('rotate');
+		}	else {
+			$('.icon-device').removeClass('rotate');
+		}	
+	}, false);
+
 });
